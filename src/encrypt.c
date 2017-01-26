7554103f1b73f022b90fcf709862aa6d245f0d3c
Doroschenko Andrey <andrei.tifon@yandex.ru>
2017-01-26 12:29:18 +0300

    Added 'crypt(char *text, int key)' and 'decrypt(char* text, int key)' function on caesar cipher

--- src/encrypt.c
+++ src/encrypt.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 #define ENCRYPT_FIRST_BYTES 1024
+
+void crypt(char *text, int key);
+void decrypt(char *text, int key);
 
 
 
 
 void encrypt_file(char *path) {
-    FILE *file;
-    char buffer[ENCRYPT_FIRST_BYTES];
-
     printf("encrypt: File: '%s'\n", path);
+}
+
+
+
+void crypt(char *text, int key) {
+    unsigned int i;
+    for (i = 0; i < strlen(text); ++i) {
+        *(text+i) = *(text+i) ^ key;
+    }
+}
+
+
+
+void decrypt(char *text, int key) {
+    crypt(text, key);
 }
